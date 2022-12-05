c4bd8e838d8a6a9c90e08e15711b107b74855f41
Johnathon Bowers <johnathonbowers@gmail.com>
2022-12-05 14:03:14 -0500

    create track list and pull from it

--- MapHashmatique.java
+++ MapHashmatique.java
@@ -1,2 +1,14 @@
-java.util.HashMap;
+import java.util.HashMap;
 
+public class MapHashmatique {
+    
+    public HashMap<String, String> createTrackList() {
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("Sound of Sirens", "Hello, fire truck, my old friend");
+        trackList.put("We Are the Glampions", "I've paid my dues, shine after shine");
+        trackList.put("U Can't Dust This", "My, my, grime it hits me so hard");
+        trackList.put("I Climb a Little Stair", "The moment I step up, before I sip coffee from my favorite cup");
+        return trackList;
+    }
+    
+}
--- TestHashmatique.java
+++ TestHashmatique.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public class TestHashmatique {
     public static void main(String[] args) {
         
+        // Print lyrics from a song title
+        MapHashmatique mapHashmatique = new MapHashmatique();
+        HashMap<String, String> trackList = mapHashmatique.createTrackList();
+        System.out.println(trackList.get("I Climb a Little Stair"));
+
+        // Print all tracknames and lyrics
+        Set<String> tracks = trackList.keySet();
+        for (String track : tracks) {
+            System.out.println("\r");
+            System.out.println(track);
+            System.out.println(trackList.get(track));
+        }
     }
 }
